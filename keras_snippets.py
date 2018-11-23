9deb6ad73ceb07ac4222064aec84bd097eaf5b68
adib0073 <adib0073@gmail.com>
2018-11-23 11:56:07 +0530

    Update keras_snippets.py

--- keras_snippets.py
+++ keras_snippets.py
@@ -5,3 +5,144 @@ model = load_model('Your-model-name.h5')
 model.summary()
 
 '''
+
+# for loading and store images in tensor in Keras 
+
+'''
+from keras.preprocessing import image
+import numpy as np
+img = image.load_img(img_path, target_size=(150, 150))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255.
+
+print(img_tensor.shape)
+'''
+
+#display pictures from image tensor
+
+'''
+import matplotlib.pyplot as plt
+plt.imshow(img_tensor[0])
+plt.show()
+'''
+
+#Instantiating a model from an input tensor and a list of output tensors
+
+'''
+from keras import models
+
+# Extract top eight layers of the selected model before flatten()
+layer_outputs = [layer.output for layer in model.layers[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(img_tensor)
+
+
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+'''
+
+# Visualizing the a particular channel
+
+'''
+import matplotlib.pyplot as plt
+plt.matshow(first_layer_activation[0, :, :, 9], cmap='viridis')
+
+# Here 0 indicates the image number selected (1st image) and 9 indicates the 9th channel or output of 4th filter
+'''
+
+# Visualizing every channel in every intermediate activation
+'''
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+images_per_row = 16
+for layer_name, layer_activation in zip(layer_names, activations):
+    n_features = layer_activation.shape[-1]
+    size = layer_activation.shape[1]
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0,
+            :, :,
+            col * images_per_row + row]
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+scale = 1. / size
+plt.figure(figsize=(scale * display_grid.shape[1],
+scale * display_grid.shape[0]))
+plt.title(layer_name)
+plt.grid(False)
+plt.imshow(display_grid, aspect='auto', cmap='viridis')
+'''
+# visualizing Filters
+'''
+from keras.applications import VGG16
+from keras import backend as K
+model = VGG16(weights='imagenet',
+include_top=False)
+layer_name = 'block3_conv1'
+filter_index = 0
+layer_output = model.get_layer(layer_name).output
+
+grads = K.gradients(loss, model.input)[0]
+grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+loss = K.mean(layer_output[:, :, :, filter_index])
+iterate = K.function([model.input], [loss, grads])
+import numpy as np
+loss_value, grads_value = iterate([np.zeros((1, 150, 150, 3))])
+
+input_img_data = np.random.random((1, 150, 150, 3)) * 20 + 128.
+step = 1.
+for i in range(40):
+    loss_value, grads_value = iterate([input_img_data])
+    input_img_data += grads_value * step
+
+def deprocess_image(x):
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+    x += 0.5
+    x = np.clip(x, 0, 1)
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+    
+def generate_pattern(layer_name, filter_index, size=150):
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:, :, :, filter_index])
+    grads = K.gradients(loss, model.input)[0]
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+    iterate = K.function([model.input], [loss, grads])
+    input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
+    step = 1.
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+    img = input_img_data[0]
+    return deprocess_image(img)
+    
+plt.imshow(generate_pattern('block3_conv1', 0))
+
+layer_name = 'block1_conv1'
+size = 64
+margin = 5
+results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3))
+for i in range(8):
+    for j in range(8):
+        filter_img = generate_pattern(layer_name, i + (j * 8), size=size)
+        horizontal_start = i * size + i * margin
+        horizontal_end = horizontal_start + size
+        vertical_start = j * size + j * margin
+        vertical_end = vertical_start + size
+        results[horizontal_start: horizontal_end,
+        vertical_start: vertical_end, :] = filter_img
+plt.figure(figsize=(20, 20))
+plt.imshow(results)
+
+'''
